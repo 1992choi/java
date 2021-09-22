ac4feb76deaaea8629799c023b2e138a07b369a8
choi <younggeunn@naver.com>
2021-09-22 19:37:41 +0900

    반복문 Label 사용

--- src/basic/control_statements/Label.java
+++ src/basic/control_statements/Label.java
@@ -6,35 +6,36 @@ import java.util.List;
 public class Label {
 
     public static void main(String[] args) {
-        List<Integer> list;
+        List<String> list;
 
         // Label을 사용하지 않은 코드
         list = new ArrayList<>();
         boolean isContinue = true;
         for (int i = 0; i < 5; i++) {
-            list.add(i);
             for (int j = 0; j < 5; j++) {
                 if (i == 2) {
                     isContinue = false;
+                    break;
                 }
+                list.add(i + "_" + j);
             }
             if (!isContinue) {
                 break;
             }
         }
-        System.out.println(list); // 0, 1, 2
+        System.out.println(list); // [0_0, 0_1, 0_2, 0_3, 0_4, 1_0, 1_1, 1_2, 1_3, 1_4]
 
         // Label을 사용한 코드
         list = new ArrayList<>();
         outerLoop:
         for (int i = 0; i < 5; i++) {
-            list.add(i);
             for (int j = 0; j < 5; j++) {
                 if (i == 2) {
                     break outerLoop;
                 }
+                list.add(i + "_" + j);
             }
         }
-        System.out.println(list); // 0, 1, 2
+        System.out.println(list); // [0_0, 0_1, 0_2, 0_3, 0_4, 1_0, 1_1, 1_2, 1_3, 1_4]
     }
 }
