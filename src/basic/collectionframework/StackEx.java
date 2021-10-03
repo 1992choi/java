9bd2f765adb4819f574ff1c310c9ac002e06304a
choi <younggeunn@naver.com>
2021-10-03 09:04:58 +0900

    Stack 구현

---
+++ src/basic/collectionframework/StackEx.java
@@ -0,0 +1,32 @@
+package basic.collectionframework;
+
+import java.util.Stack;
+
+public class StackEx {
+
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        // 데이터 추가
+        stack.push("가");
+        stack.push("나");
+        stack.push("다");
+        stack.push("라");
+        stack.push("마");
+        System.out.println(stack); // [가, 나, 다, 라, 마]
+
+        // TOP 확인
+        System.out.println(stack.peek()); // 마
+        System.out.println(stack); // [가, 나, 다, 라, 마]
+
+        // 데이터 추출
+        System.out.println(stack.pop()); // 마
+        System.out.println(stack.pop()); // 라
+        System.out.println(stack); // [가, 나, 다]
+        
+        // 데이터 검색
+        System.out.println(stack.search("다")); // 1 : TOP부터 1로 순서가 할당된다.
+        System.out.println(stack.search("가")); // 3
+        System.out.println(stack.search("하")); // -1 : 없으면 -1 리턴
+    }
+
+}
