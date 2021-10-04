e94936ebf6203d977008bcdb6667fbcec89aafaf
choi <younggeunn@naver.com>
2021-10-04 17:58:51 +0900

    BufferedReader 예제

---
+++ src/basic/io/BufferedReaderEx.java
@@ -0,0 +1,31 @@
+package basic.io;
+
+import java.io.*;
+
+public class BufferedReaderEx {
+
+    public static void main(String[] args) throws IOException {
+        /*
+            Reader(FileReader, BufferedReader, InputStreamReader)와
+            Writer(FileWriter, BufferedWriter, OutputStreamWriter)는
+            기존 예제(xxxStreamEx)와 다르게 byte단위의 입출력이 아닌 char단위의 입출력으로 문자열 입출력을 위해 특화된 기법이다.
+         */
+        File file = new File(System.getProperty("user.dir") + File.separator + "data" + File.separator + "xxxReader.txt");
+        Reader reader = new FileReader(file);
+        BufferedReader br = new BufferedReader(reader);
+        String data;
+        while ((data = br.readLine()) != null) {
+            System.out.println(data);
+            /*
+                실행결과
+                : 123
+                  abc
+                  가나다
+             */
+        }
+
+        br.close();
+        reader.close();
+    }
+
+}
