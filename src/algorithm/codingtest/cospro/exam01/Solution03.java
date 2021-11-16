12c251cb5c1434e0c3a09b86dfffc984a9f60b44
choi <younggeunn@naver.com>
2021-11-16 22:31:15 +0900

    COS Pro 샘플문제 - 문제 commit

---
+++ src/algorithm/codingtest/cospro/exam01/Solution03.java
@@ -0,0 +1,20 @@
+package algorithm.codingtest.cospro.exam01;
+
+public class Solution03 {
+
+    public int solution(String pos) {
+        // Write code here.
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution03 sol = new Solution03();
+        String pos = "A7";
+        int ret = sol.solution(pos);
+
+        // Press Run button to receive output.
+        System.out.println("Solution: return value of the method is " + ret + " .");
+    }
+
+}
