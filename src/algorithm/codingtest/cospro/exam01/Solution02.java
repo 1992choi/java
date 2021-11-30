da71ed0a66d0dde76c73405db33b2c457959023b
choi <younggeunn@naver.com>
2021-11-30 21:24:55 +0900

    COS Pro 샘플문제 - 1차 2번

--- src/algorithm/codingtest/cospro/exam01/Solution02.java
+++ src/algorithm/codingtest/cospro/exam01/Solution02.java
@@ -2,26 +2,78 @@ package algorithm.codingtest.cospro.exam01;
 
 public class Solution02 {
 
-//    public int solution(int n) {
-//        // Write code here.
-//        int answer = 0;
-//        return answer;
-//    }
-//
-//    public static void main(String[] args) {
-//        Solution02 sol = new Solution02();
-//        int n1 = 3;
-//        int ret1 = sol.solution(n1);
-//
-//
-//        // Press Run button to receive output.
-//        System.out.println("Solution: return value of the method is " + ret1 + " .");
-//
-//        int n2 = 2;
-//        int ret2 = sol.solution(n2);
-//
-//        // Press Run button to receive output.
-//        System.out.println("Solution: return value of the method is " + ret2 + " .");
-//    }
+    public int solution(int n) {
+        System.out.println("========================== " + n + "*" + n + " Array ==========================");
+        int answer = 0;
+
+        int[][] arr = new int[n][n];
+        int fillCnt = 1;
+        int direction = 1; //0-N, 1-E, 2-S, 3-W
+        int x = 0;
+        int y = 0;
+        boolean isContinue = true;
+
+        do {
+            if (direction == 1) {
+                if (x >= n || arr[y][x] != 0) {
+                    direction = 2;
+                    x--;
+                    y++;
+                } else {
+                    arr[y][x++] = fillCnt++;
+                }
+            } else if (direction == 2) {
+                if (y >= n || arr[y][x] != 0) {
+                    direction = 3;
+                    y--;
+                    x--;
+                } else {
+                    arr[y++][x] = fillCnt++;
+                }
+            } else if (direction == 3) {
+                if (x < 0 || arr[y][x] != 0) {
+                    direction = 0;
+                    y--;
+                    x++;
+                } else {
+                    arr[y][x--] = fillCnt++;
+                }
+            } else if (direction == 0) {
+                if (y < 0 || arr[y][x] != 0) {
+                    direction = 1;
+                    x++;
+                    y++;
+                } else {
+                    arr[y--][x] = fillCnt++;
+                }
+            }
+
+            if (fillCnt - 1 == n * n) {
+                isContinue = false;
+            }
+        } while(isContinue);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    answer += arr[i][j];
+                }
+                System.out.printf("%5d", arr[i][j]);
+            }
+            System.out.println();
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution02 sol = new Solution02();
+
+        System.out.println("sum : " + sol.solution(1));
+        System.out.println("sum : " + sol.solution(2));
+        System.out.println("sum : " + sol.solution(3));
+        System.out.println("sum : " + sol.solution(5));
+        System.out.println("sum : " + sol.solution(10));
+    }
 
 }
