1b1e6434a81f0e794c5a476fbcad11466fbd5e37
choi <younggeunn@naver.com>
2021-12-24 19:29:16 +0900

    프로그래머스 Level1 - 최대공약수와 최소공배수

--- src/algorithm/codingtest/programmers/level1/최대공약수와_최소공배수.java
+++ src/algorithm/codingtest/programmers/level1/최대공약수와_최소공배수.java
@@ -8,11 +8,8 @@ public class 최대공약수와_최소공배수 {
     public static int[] solution(int n, int m) {
         int[] answer = new int[2];
 
-        int max = Math.max(n, m);
-        int min = Math.min(n, m);
-
         // 최대공약수
-        for (int i = 1; i <= min; i++) {
+        for (int i = 1; i <= Math.min(n, m); i++) {
             if (n % i == 0 && m % i == 0) {
                 answer[0] = i;
             }
