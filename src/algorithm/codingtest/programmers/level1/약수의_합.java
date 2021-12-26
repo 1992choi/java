b3cd3cc665eea871f4aa541602f13ba537c3fab9
choi <younggeunn@naver.com>
2021-12-26 09:47:51 +0900

    프로그래머스 Level1 - 약수의 합

---
+++ src/algorithm/codingtest/programmers/level1/약수의_합.java
@@ -0,0 +1,22 @@
+package algorithm.codingtest.programmers.level1;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12928
+public class 약수의_합 {
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(12));
+        System.out.println(solution(5));
+    }
+}
