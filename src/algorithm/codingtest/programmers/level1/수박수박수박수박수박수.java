47ba4ea1de816c3bd9d068e246b1018d86587334
choi <younggeunn@naver.com>
2021-12-26 13:30:42 +0900

    프로그래머스 Level1 - 수박수박수박수박수박수

---
+++ src/algorithm/codingtest/programmers/level1/수박수박수박수박수박수.java
@@ -0,0 +1,25 @@
+package algorithm.codingtest.programmers.level1;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12922
+public class 수박수박수박수박수박수 {
+
+    public static String solution(int n) {
+        String answer = "";
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                answer += "수";
+            } else {
+                answer += "박";
+            }
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(3));
+        System.out.println(solution(4));
+    }
+}
