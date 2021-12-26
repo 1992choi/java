4b73d66d72c12a18e02aa57ce0605d136856c4b6
choi <younggeunn@naver.com>
2021-12-26 10:58:51 +0900

    프로그래머스 Level1 - 시저 암호

---
+++ src/algorithm/codingtest/programmers/level1/시저_암호.java
@@ -0,0 +1,26 @@
+package algorithm.codingtest.programmers.level1;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12926
+public class 시저_암호 {
+
+    public static String solution(String s, int n) {
+        String result = "";
+        n = n % 26;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                ch = (char) ((ch - 'a' + n) % 26 + 'a');
+            } else if (Character.isUpperCase(ch)) {
+                ch = (char) ((ch - 'A' + n) % 26 + 'A');
+            }
+            result += ch;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("AB", 1));
+        System.out.println(solution("z", 1));
+        System.out.println(solution("a B z", 4));
+    }
+}
