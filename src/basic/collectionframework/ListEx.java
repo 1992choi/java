dc97678b1150015b3da06b249d5cfff5daacd28a
choi <younggeunn@naver.com>
2022-06-20 22:21:52 +0900

    ArrayList 예제 추가

--- src/basic/collectionframework/ListEx.java
+++ src/basic/collectionframework/ListEx.java
@@ -37,12 +37,55 @@ public class ListEx {
         System.out.println(arrayList); // [가, 다]
         System.out.println(arrayList.size()); // 2
         System.out.println(arrayList.isEmpty()); // false
-        
+
         // 초기화 후
         arrayList.clear();
         System.out.println(arrayList); // []
         System.out.println(arrayList.size()); // 0
         System.out.println(arrayList.isEmpty()); // true
+
+        // 인덱스 및 값을 통한 요소 삭제2
+        List list2 = null;
+        // 인덱스로 삭제
+        list2 = new ArrayList<>();
+        list2.add("1");
+        list2.add(2);
+        list2.add(1);
+        System.out.println(list2); // [1, 2, 1]
+        list2.remove(1);
+        System.out.println(list2); // [1, 1] : 1번째 인덱스인 2가 삭제됨.
+        // 요소로 삭제
+        list2 = new ArrayList<>();
+        list2.add("1");
+        list2.add(2);
+        list2.add(1);
+        System.out.println(list2); // [1, 2, 1]
+        list2.remove(new Integer(1));
+        System.out.println(list2); // [1, 2] : 숫자 1이 삭제됨.
+
+        // A.retainAll(B) : A의 요소 중 B에 존재하는 요소만 남기고 삭제한다.
+        List<String> listA = null;
+        List<String> listB = null;
+        // listA.retainAll(listB);
+        listA = new ArrayList<>();
+        listA.add("A"); listA.add("B"); listA.add("C"); listA.add("H");
+        listB = new ArrayList<>();
+        listB.add("B"); listB.add("C"); listB.add("D");
+        System.out.println(listA); // [A, B, C, H]
+        System.out.println(listB); // [B, C, D]
+        listA.retainAll(listB);
+        System.out.println(listA); // [B, C]
+        System.out.println(listB); // [B, C, D]
+        // listB.retainAll(listA);
+        listA = new ArrayList<>();
+        listA.add("A"); listA.add("B"); listA.add("C"); listA.add("H");
+        listB = new ArrayList<>();
+        listB.add("B"); listB.add("C"); listB.add("D");
+        System.out.println(listA); // [A, B, C, H]
+        System.out.println(listB); // [B, C, D]
+        listB.retainAll(listA);
+        System.out.println(listA); // [A, B, C, H]
+        System.out.println(listB); // [B, C]
 
 
         /* Vector - ArrayList와 유사하나 동기화 메서드로 구현되어 있어 멀티 쓰레드에 적합하다. */
@@ -54,7 +97,8 @@ public class ListEx {
 
         /*
             ArrayList와 LinkedList 성능비교
-            1. ArrayList    - 검색 속도 빠름, 추가&삭제 느림
+            1. ArrayList    - 검색 속도 빠름, 추가&삭제 느림(순차적 추가&삭제는 더 빠르다.)
+                               * ArrayList의 요소를 순차적으로 삭제할 경우, 뒤에서부터 순차적으로 삭제한다면 배열의 복사가 발생하지 않는다.
             2. LinkedList   - 검색 속도 느림, 추가&삭제 빠름
          */
     }
