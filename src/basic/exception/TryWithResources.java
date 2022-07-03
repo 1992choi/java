ffb757be9b1d41d602689c5cd969030bd7c14059
choi <younggeunn@naver.com>
2022-07-03 09:29:11 +0900

    Try-with-resources 예제

--- src/basic/exception/TryWithResources.java
+++ src/basic/exception/TryWithResources.java
@@ -1,12 +1,54 @@
 package basic.exception;
 
+import java.io.Closeable;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+class ClassImplementsCloseable implements Closeable {
+    @Override
+    public void close() throws IOException {
+        System.out.println("Call ClassImplementsCloseable");
+    }
+}
+
+class ClassNotImplementsCloseable {
+}
+
+class ClassHavingCustomClose {
+    public void close() throws IOException {
+        System.out.println("Call ClassHavingCustomClose");
+    }
+}
+
 public class TryWithResources {
 
     public static void main(String[] args) {
+        /* Try-with-resources 구문은 Closeable 인터페이스를 구현한 클래스만 가능하다. */
+        // 1. Closeable 인터페이스를 구현한 클래스
+        try (ClassImplementsCloseable class1 = new ClassImplementsCloseable()) {
+
+        } catch (Exception e) {
+
+        }
+
+        // 2. Closeable 인터페이스를 구현하지 않은 클래스 : 오류 발생
+//        try (ClassNotImplementsCloseable class2 = new ClassNotImplementsCloseable()) {
+//
+//        } catch (Exception e) {
+//
+//        }
+
+        // 3. Closeable 인터페이스를 구현하지 않은 클래스(이름이 동일한 close 메서드만 있는 클래스) : 오류 발생
+//        try (ClassHavingCustomClose class3 = new ClassHavingCustomClose()) {
+//
+//        } catch (Exception e) {
+//
+//        }
+
+
+
+        /* 자원 해제방식 비교 */
         // 기본적인 자원 해제 방식
         FileInputStream fis = null;
         FileOutputStream fos = null;
