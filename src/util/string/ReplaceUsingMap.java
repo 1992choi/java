b1b52aa5e14d0b99c92c078f3b4927ac26ab98cf
choi <younggeunn@naver.com>
2022-08-11 21:53:54 +0900

    문자열 내 특정 단어 변경

--- src/util/string/ReplaceUsingMap.java
+++ src/util/string/ReplaceUsingMap.java
@@ -7,6 +7,7 @@ import java.util.Map;
 public class ReplaceUsingMap {
 
     public static void main(String[] args) {
+        String originValue = "";
         Map<String, String> map = new HashMap();
         map.put("code0", "코드0");
         map.put("code1", "코드1");
@@ -15,17 +16,30 @@ public class ReplaceUsingMap {
         map.put("code4", "코드4");
         map.put("code5", "코드5");
 
-        // 변환 전 문자열
-        String str = "[code0 -> code1], [code2 -> code3], [code4 -> code5]";
-        System.out.println(str); // [code0 -> code1], [code2 -> code3], [code4 -> code5]
+        /*** 변환 1. ***/
+        // 변경 전
+        originValue = "[code0 -> code1], [code2 -> code3], [code4 -> code5]";
+        System.out.println(originValue); // [code0 -> code1], [code2 -> code3], [code4 -> code5]
 
-        // 변환 후 문자열
+        // 변경 후
         Iterator<String> it = map.keySet().iterator();
         while (it.hasNext()) {
             String key = it.next();
-            str = str.replaceAll(key, map.get(key));
+            originValue = originValue.replaceAll(key, map.get(key));
         }
-        System.out.println(str); // [코드0 -> 코드1], [코드2 -> 코드3], [코드4 -> 코드5]
+        System.out.println(originValue); // [코드0 -> 코드1], [코드2 -> 코드3], [코드4 -> 코드5]
+
+
+        /*** 변환 2. ***/
+        // 변경 전
+        originValue = "[code0 -> code1], [code2 -> code3], [code4 -> code5]";
+        System.out.println(originValue); // [code0 -> code1], [code2 -> code3], [code4 -> code5]
+
+        // 변경 후
+        for (String key : map.keySet()) {
+            originValue = originValue.replaceAll(key, map.get(key));
+        }
+        System.out.println(originValue); // [코드0 -> 코드1], [코드2 -> 코드3], [코드4 -> 코드5]
     }
 
 }
