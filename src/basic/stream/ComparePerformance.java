cec5d4059960c5334bca58830287f782b76ae306
choi <younggeunn@naver.com>
2022-08-25 00:11:46 +0900

    Stream API 성능비교

--- src/basic/stream/ComparePerformance.java
+++ src/basic/stream/ComparePerformance.java
@@ -6,19 +6,19 @@ import java.util.List;
 public class ComparePerformance {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 1_000_000; i++) {
+        List<Long> list = new ArrayList<>();
+        for (long i = 0; i < 10_000_000; i++) {
             list.add(i);
         }
 
         // for 문으로 합계 구하기
         long start = System.currentTimeMillis();
-        int size = list.size();
-        int sum = 0;
+        long size = list.size();
+        long sum = 0;
         for (int i = 0; i < size; i++) {
             sum += list.get(i);
         }
-        System.out.println("sum = " + sum + " [" + (System.currentTimeMillis() - start) + "]"); // sum = 1783293664 [4]
+        System.out.println("sum = " + sum + " [" + (System.currentTimeMillis() - start) + "ms]"); // sum = 49999995000000 [18ms]
 
         // for 문으로 합계 구하기 [list.size() 그대로 사용]
         start = System.currentTimeMillis();
@@ -26,20 +26,20 @@ public class ComparePerformance {
         for (int i = 0; i < list.size(); i++) {
             sum += list.get(i);
         }
-        System.out.println("sum = " + sum + " [" + (System.currentTimeMillis() - start) + "]"); // sum = 1783293664 [6]
+        System.out.println("sum = " + sum + " [" + (System.currentTimeMillis() - start) + "ms]"); // sum = 49999995000000 [15ms]
 
         // for-each로 합계 구하기
         start = System.currentTimeMillis();
         sum = 0;
-        for (int i : list) {
-            sum += list.get(i);
+        for (long l : list) {
+            sum += l;
         }
-        System.out.println("sum = " + sum + " [" + (System.currentTimeMillis() - start) + "]"); // sum = 1783293664 [5]
+        System.out.println("sum = " + sum + " [" + (System.currentTimeMillis() - start) + "ms]"); // sum = 49999995000000 [15ms]
 
-        // for-each로 합계 구하기
+        // Stream API로 합계 구하기
         start = System.currentTimeMillis();
-        sum = list.stream().reduce(0, Integer::sum);
-        System.out.println("sum = " + sum + " [" + (System.currentTimeMillis() - start) + "]"); // sum = 1783293664 [38]
+        sum = list.stream().reduce(0L, Long::sum);
+        System.out.println("sum = " + sum + " [" + (System.currentTimeMillis() - start) + "ms]"); // sum = 49999995000000 [69ms]
     }
 
 }
