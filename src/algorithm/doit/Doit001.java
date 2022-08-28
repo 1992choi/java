e1475f229db2b866671d9e6d0de22a08fb7447d5
choi <younggeunn@naver.com>
2022-08-28 16:20:01 +0900

    Do it! 알고리즘 풀이

--- src/algorithm/doit/Doit001.java
+++ src/algorithm/doit/Doit001.java
@@ -1,12 +1,12 @@
 package algorithm.doit;
 
-/*
-    문자열이 주어질 때, 각 자리의 합 구하기
-    count = 숫자 갯수
-    str = 숫자로 구성된 문자열
- */
 public class Doit001 {
 
+    /*
+        문자열이 주어질 때, 각 자리의 합 구하기
+        count = 숫자 갯수
+        str = 숫자로 구성된 문자열
+     */
     static int solution(int count, String str) {
         int answer = 0;
         for (int i = 0; i < count; i++) {
