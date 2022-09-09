1eb61b0d6a71847c764aca457a32f83d777d7305
choi <younggeunn@naver.com>
2022-09-09 17:26:20 +0900

    Do it! 알고리즘 풀이

--- src/algorithm/doit/Doit016.java
+++ src/algorithm/doit/Doit016.java
@@ -26,10 +26,10 @@ public class Doit016 {
     static int solution(int[] arr) {
         boolean changed = false;
         int N = arr.length - 1;
-        for (int i=1; i<=N+1; i++) {
+        for (int i = 1; i <= N + 1; i++) {
             changed = false;
-            for (int j=1; j<=N-i; j++) {
-                if (arr[j] > arr[j+1]) {
+            for (int j = 1; j <= N - i; j++) {
+                if (arr[j] > arr[j + 1]) {
                     changed = true;
                 }
             }
