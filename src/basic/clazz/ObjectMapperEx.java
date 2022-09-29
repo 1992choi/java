4854d83b557cada6f0dc54baf49227147ed4a2fd
choi <younggeunn@naver.com>
2022-09-29 22:30:28 +0900

    ObjectMapper 예제

---
+++ src/basic/clazz/ObjectMapperEx.java
@@ -0,0 +1,72 @@
+package basic.clazz;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Player {
+
+    int age;
+    String name;
+
+    public Player(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + "(" + age + ")";
+    }
+}
+
+public class ObjectMapperEx {
+
+    public static void main(String[] args) {
+        /*
+            List<Object> -> List<Map>
+         */
+
+        // 데이터 생성
+        List<Player> players = new ArrayList<>();
+        makeData(players);
+        System.out.println(players); // [choi(31), lee(20), kim(22), park(40)]
+
+        // 데이터 변환
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Map<String, Object>> playerList = new ArrayList<>();
+        for (Player player : players) {
+            HashMap<String, Object> map = objectMapper.convertValue(player, HashMap.class);
+            playerList.add(map);
+        }
+        System.out.println(playerList); // [{name=choi, age=31}, {name=lee, age=20}, {name=kim, age=22}, {name=park, age=40}]
+
+    }
+
+    public static void makeData(List<Player> players){
+        players.add(new Player(31, "choi"));
+        players.add(new Player(20, "lee"));
+        players.add(new Player(22, "kim"));
+        players.add(new Player(40, "park"));
+    }
+
+}
