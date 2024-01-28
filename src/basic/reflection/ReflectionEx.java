c140a1ca22c692697456d9d4bfb8c0c8561b3494
choi <younggeunn@naver.com>
2024-01-28 18:12:12 +0900

    reflect 예제

--- src/basic/reflection/ReflectionEx.java
+++ src/basic/reflection/ReflectionEx.java
@@ -5,30 +5,30 @@ import java.lang.reflect.Method;
 public class ReflectionEx {
 
     public static void main(String[] args) throws Exception {
-        Sample sample = new Sample();
-        String param = "callA";
+        Theme theme = new Theme();
+        String userSelectTheme = "Blue";
 
         // 직접 호출하는 경우
-        if ("callA".equals(param)) {
-            sample.callA(); // Run callA()
-        } else {
-            sample.callB(); // Run callB()
+        if ("Blue".equals(userSelectTheme)) {
+            theme.changeBlue(); // 파란 테마로 변경
+        } else if ("Red".equals(userSelectTheme)) {
+            theme.changeRed(); // 빨간 테마로 변경
         }
 
         // Reflection을 사용하는 경우 : 클래스나 메서드 정보를 동적으로 변경할 수 있다는 장점이 있다.
-        Class clazz = Class.forName("basic.reflection.Sample");
-        Method method = clazz.getMethod(param);
-        method.invoke(sample); // Run callA() : '직접 호출하는 경우'와 다르게 메서드를 실행하는 로직을 공통으로 만들 수 있다.
+        Class clazz = Class.forName("basic.reflection.Theme");
+        Method method = clazz.getMethod("change" + userSelectTheme);
+        method.invoke(theme); // 파란 테마로 변경 : '직접 호출하는 경우'와 다르게 메서드를 실행하는 로직을 공통으로 만들 수 있다.
     }
 
 }
 
-class Sample {
-    public void callA() {
-        System.out.println("Run callA()");
+class Theme {
+    public void changeBlue() {
+        System.out.println("파란 테마로 변경");
     }
 
-    public void callB() {
-        System.out.println("Run callB()");
+    public void changeRed() {
+        System.out.println("빨간 테마로 변경");
     }
 }
