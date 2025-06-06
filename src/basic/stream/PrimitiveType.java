6e04c7db3f59f6b84216627ecf6483cab9fd5c37
choi <younggeunn@naver.com>
2025-06-06 14:34:05 +0900

    기본형 스트림 - 주요 기능 및 메서드 추가

--- src/basic/stream/PrimitiveType.java
+++ src/basic/stream/PrimitiveType.java
@@ -1,5 +1,6 @@
 package basic.stream;
 
+import java.util.IntSummaryStatistics;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -18,21 +19,42 @@ public class PrimitiveType {
               : LongStream
         */
 
-        // 일반 스트림
+        /* 일반 스트림 */
         Stream stream = Stream.of(1, 2, 3, 4, 5);
         System.out.println(stream.count()); // 5
         // sum(), max() 등 미제공 : 숫자형 타입의 객체라는 보장이 없으므로...
 
 
-        // IntStream : 숫자 관련 메서드는 일반 스트림보다 더 많은 기능을 제공한다.
+        /* IntStream : 숫자 관련 메서드는 일반 스트림보다 더 많은 기능을 제공한다. */
+        // 범위 지정 생성
+        System.out.println("\n\n--- range ---");
+        IntStream.range(1, 5)
+                .forEach(i -> System.out.print(i + " ")); // 1 2 3 4 : 시작 ~ 끝 미만
+
+        System.out.println("\n\n--- rangeClosed ---");
+        IntStream.rangeClosed(1, 5)
+                .forEach(i -> System.out.print(i + " ")); // 1 2 3 4 5 : 시작 ~ 끝 이하
+
+        // 주요 기능 및 메서드
+        System.out.println("\n\n--- count ---");
         IntStream intStream = IntStream.of(1, 2, 3, 4, 5);
         System.out.println(intStream.count()); // 5
 
+        System.out.println("\n\n--- sum ---");
         intStream = IntStream.of(1, 2, 3, 4, 5);
         System.out.println(intStream.sum()); // 15
 
+        System.out.println("\n\n--- max ---");
         intStream = IntStream.of(1, 2, 3, 4, 5);
         System.out.println(intStream.max().getAsInt()); // 5
+
+        System.out.println("\n\n--- 집계 ---");
+        IntSummaryStatistics intSummaryStatistics = IntStream.of(1, 2, 3, 4, 5).summaryStatistics();
+        System.out.println(intSummaryStatistics.getCount()); // 5
+        System.out.println(intSummaryStatistics.getSum()); // 15
+        System.out.println(intSummaryStatistics.getAverage()); // 3.0
+        System.out.println(intSummaryStatistics.getMax()); // 5
+        System.out.println(intSummaryStatistics.getMin()); // 1
     }
 
 }
